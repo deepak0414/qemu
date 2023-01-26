2b73dae30a7208d05bea7886b20022c1eab0523b
Deepak Gupta <debug@rivosinc.com>
2023-01-26 11:26:31 -0800

    Zisslpcfi: Instructions encodings, implementation and handlers
    This patch implements instruction encodings for Zisslpcfi instructions.
    Additionally this patch implements Zimops encodings as well. If Zisslpcfi
    is supported by CPU but not enabled then all Zisslpcfi instructions
    default to Zimops instuction behavior i.e. mov 0 to rd.
    
    Zisslpcfi defines following instructions.
    - Backward control flow
        - sspush x1/x5 : Decrement shadow stack pointer and pushes x1 or x5
                         on shadow stack.
        - sspop x1/x5 : Pops from shadow stack into x1 or x5. Increments
                        shadow stack pointer.
        - ssprr : Reads current shadow stack pointer into a destination
                  register.
        - sscheckra : Compares x1 with x5. Raises illegal instr fault if
                    x1 != x5
        - ssamoswap : Atomically swaps value on top of shadow stack
    
    - Forward control flow
        - lpsll, lpsml, lpsul : sets lower (9bit), mid (8bit) and upper
                                (8bit) label values in CSR_ULLP respectively.
        - lpcll, lpcml, lpcul : checks lower (9bit), mid (8bit) and upper
                                (8bit) label values with CSR_ULLP
                                respectively.
        Check label instructions raise illegal instruction fault when labels
        mismatch.
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Signed-off-by: Kip Walker  <kip@rivosinc.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -1106,6 +1106,8 @@ static uint32_t opcode_at(DisasContextBase *dcbase, target_ulong pc)
 #include "decode-xthead.c.inc"
 #include "insn_trans/trans_xthead.c.inc"
 #include "insn_trans/trans_xventanacondops.c.inc"
+#include "insn_trans/trans_zisslpcfi.c.inc"
+#include "insn_trans/trans_zimops.c.inc"
 
 /* Include the auto-generated decoder for 16 bit insn */
 #include "decode-insn16.c.inc"
