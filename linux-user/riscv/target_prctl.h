3dc577f142d7697a1c5dcfff46c081f8106f7acc
Deepak Gupta <debug@rivosinc.com>
2024-07-18 14:56:01 -0700

    linux-user/riscv: implement indirect branch tracking prctls
    Implements indirect branch tracking prctls for riscv. Setting and clearing
    branch tracking prctl simply turns on/off `ufcfien` field in `env`.
    tb flush is needed because branch tracking itself leverages tb creation
    logic.
    
    locking branch tracking (forward cfi) is not implemented yet (no need yet)
    but added for completeness (kernel patches have this prctl)
    
    Signed-off-by: Deepak Gupta <debug@rivosinc.com>
    Co-developed-by: Jesse Huang <jesse.huang@sifive.com>
    Co-developed-by: Jim Shu <jim.shu@sifive.com>
    Co-developed-by: Andy Chiu <andy.chiu@sifive.com>

--- linux-user/riscv/target_prctl.h
+++ linux-user/riscv/target_prctl.h
@@ -1,1 +1,45 @@
 /* No special prctl support required. */
+#ifndef RISCV_TARGET_PRCTL_H
+#define RISCV_TARGET_PRCTL_H
+
+/*
+ * -TARGET_EINVAL: Unsupported/Invalid flag for this architecture
+ * -TARGET_EACCES: try to set an already set CFI feature
+ * -TARGET_ENOENT: CFI feature is not supported by CPU
+ */
+static abi_long do_prctl_cfi(CPUArchState *env,
+                                 abi_long option, abi_long flag)
+{
+    if (env_archcpu(env)->cfg.ext_zicfilp) {
+
+        switch (option) {
+        case PR_GET_INDIR_BR_LP_STATUS:
+            abi_ulong fcfi_status = 0;
+            /* indirect branch tracking is enabled on the task or not */
+            if (env->senvcfg & SENVCFG_LPE) {
+                fcfi_status |= PR_INDIR_BR_LP_ENABLE;
+            }
+            return copy_to_user(flag, &fcfi_status, sizeof(fcfi_status)) ? \
+                   -EFAULT : 0;
+
+        case PR_SET_INDIR_BR_LP_STATUS:
+            /* if any other bit is set, its invalid param */
+            if (flag & ~PR_INDIR_BR_LP_ENABLE) {
+                return -TARGET_EINVAL;
+            }
+            /* set or clear branch tracking */
+            env->senvcfg |= (flag & PR_INDIR_BR_LP_ENABLE) ? SENVCFG_LPE : 0;
+            return 0;
+
+        /* locking not implemented (also not needed for qemu-user) yet */
+        case PR_LOCK_INDIR_BR_LP_STATUS:
+            return -TARGET_EINVAL;
+        }
+    }
+
+    return -TARGET_ENOENT;
+}
+
+#define do_prctl_cfi do_prctl_cfi
+
+#endif
